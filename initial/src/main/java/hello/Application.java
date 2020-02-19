2f92f956fb6c02ff55847e2e4edc89cc845d33f0
James Hines <jameshines10@gmail.com>
2020-02-19 18:59:12 +0000

    Adds a simple controller class

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World!";
+  }
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
